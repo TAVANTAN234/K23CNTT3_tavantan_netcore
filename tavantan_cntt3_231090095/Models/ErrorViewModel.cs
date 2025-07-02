d5ca7aa516d0f49928ea63f33ce59dac7b10a3ca
tan van <Tavantan113355@gmail.com>
2025-07-02 11:22:50 +0700

    tavantan_cntt3_231090095

---
+++ tavantan_cntt3_231090095/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace tavantan_cntt3_231090095.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
